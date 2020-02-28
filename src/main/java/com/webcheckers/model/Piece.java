c79e6801fbd056c11a02e66848afc3e667fb5c7c
anb6536 <anb6536@rit.edu>
2020-02-28 13:31:30 -0500

    Merge branch 'GameStart' of C:\Users\aahis\Documents\College\SWEN 261\Project\team-project-2195-swen-261-12-a-teama with conflicts.

---
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -0,0 +1,8 @@
+package com.webcheckers.model;
+
+public class Piece {
+    public enum Color{
+        RED,
+        WHITE
+    }
+}
--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,23 +1,22 @@
 package com.webcheckers.ui;
 
 import com.google.gson.Gson;
-import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Player;
-import com.webcheckers.model.PlayerTypes;
-import com.webcheckers.model.UserModel;
 import spark.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.logging.Logger;
-
 import static com.webcheckers.model.Game.Mode.PLAY;
-
+import static com.webcheckers.model.Piece.Color.*;
 public class GetGameRoute implements Route {
 
     private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
     private final TemplateEngine templateEngine;
     private Gson gson;
 
+    private static final String TITLE = "title";
     private static final String CURRENT_USER = "currentUser";
     private static final String GAME_ID = "gameID";
     private static final String VIEW_MODE = "viewMode";
@@ -25,74 +24,28 @@ public class GetGameRoute implements Route {
     private static final String RED_PLAYER = "redPlayer";
     private static final String WHITE_PLAYER = "whitePlayer";
     private static final String ACTIVE_COLOR = "activeColor";
-    private static final String TITLE = "title";
-    public static final String USERATTRIB = "UserAttrib";
-    private PlayerLobby playerLobby;
 
-    public GetGameRoute(TemplateEngine templateEngine, PlayerLobby lobby) {
+    public GetGameRoute(TemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
-        this.playerLobby = lobby;
         this.gson = new Gson();
         LOG.config("GetGameRoute is Initialized");
     }
 
 
-    /**
-     * handle all requests where we are in the game
-     *
-     * @param request  idek TODO
-     * @param response idek TODO
-     * @return the templateEngine? TODO
-     * @throws Exception TODO why?
-     */
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        // get the current session
-        Session session = request.session();
-
-        // get the current player object and opponent name
-        Player player = session.attribute(USERATTRIB);
-        //TODO error checking (if someone types in the url ?opponent=null)
-        String opponentName = request.queryParamsValues("opponent")[0];
-        Map<String, Player> playerObjects = playerLobby.getPlayerObjects();
-        Player opponent = playerObjects.get(opponentName);
-
-        // make sure this opponent playerName pair are registered
-        if (!GetHomeRoute.playersInGame.containsKey(player.getname())) {
-            if (!UserModel.playersWithTypes.containsKey(player.getname())) {
-                // decide who is red and who is white
-                if (new Random().nextDouble() < 0.5) {
-                    UserModel.playersWithTypes.put(player.getname(), PlayerTypes.types.RED);
-                    UserModel.playersWithTypes.put(opponentName, PlayerTypes.types.WHITE);
-                }
-            }
-            GetHomeRoute.playersInGame.put(player.getname(), opponentName);
-        }
-
-        // check who is red and who is white
-        Player redPlayer;
-        Player whitePlayer;
-        if (0 == UserModel.playersWithTypes.get(player.getname()).compareTo(PlayerTypes.types.RED)) {
-            redPlayer = player;
-            whitePlayer = opponent;
-        } else {
-            whitePlayer = player;
-            redPlayer = opponent;
-        }
-
-
-        // make a new vm to put values in and populate the values
         Map<String, Object> vm = new HashMap<>();
+        Session session = request.session();
+        vm.put(TITLE, "Welcome to the Game of WEBCHECKERS");
+
+        Player player = session.attribute(CURRENT_USER);
         vm.put(CURRENT_USER, player);
         String gameId = session.attribute(GAME_ID);
         vm.put(GAME_ID, gameId);
         vm.put(VIEW_MODE, PLAY);
-        vm.put(RED_PLAYER, redPlayer);
-        vm.put(WHITE_PLAYER, whitePlayer);
-        vm.put(MODE_OPTIONS, this.gson);
-        vm.put(ACTIVE_COLOR, "Blue"); // TODO figure out what active color is
-        vm.put(TITLE, "descriptvie title");
-
+        vm.put(RED_PLAYER, session.attribute(WHITE_PLAYER));
+        vm.put(WHITE_PLAYER, session.attribute(RED_PLAYER));
+        vm.put(ACTIVE_COLOR, RED);
 
         return templateEngine.render(new ModelAndView(vm, "game.ftl"));
     }
