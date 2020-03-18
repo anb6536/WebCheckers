34893369af4d6c5972943ae5fb2490e5f8c50a21
anb6536 <anb6536@rit.edu>
2020-03-18 11:34:28 -0400

    Completed the test where the type and the color of the piece are tested.

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.webcheckers.model.Piece.Type.SINGLE;
+import static org.junit.jupiter.api.Assertions.*;
+
 @Tag("Model-Tier")
 public class PieceTest {
     private Piece redPiece;
@@ -17,11 +20,19 @@ public class PieceTest {
 
     @Test
     void getType(){
+        assertNotNull(redPiece.getType(), "Piece Type is accessible");
+        assertNotNull(whitePiece.getType(), "Piece Type is accessible");
 
+        assertEquals(redPiece.getType(), SINGLE, "Red Piece is of Single type");
+        assertEquals(whitePiece.getType(), SINGLE, "White Piece is of Single type");
     }
 
     @Test
     void getColor(){
-        
+        assertEquals(redPiece.getColor(), Piece.Color.RED, "Color of the Red piece is Red");
+        assertEquals(whitePiece.getColor(), Piece.Color.WHITE, "Color of the White piece is White");
+
+        assertNotEquals(redPiece.getColor(), Piece.Color.WHITE, "Color of the Red piece is not White");
+        assertNotEquals(whitePiece.getColor(), Piece.Color.RED, "Color of the White piece is not Red");
     }
 }
