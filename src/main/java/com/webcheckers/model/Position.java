8e1607183e8410be2f18a5e442b62375a90d5ad1
HugGa <galloh@bxscience.edu>
2020-03-26 13:38:52 -0400

    Add hashCode and equals and quiet java for a bit

--- src/main/java/com/webcheckers/model/Position.java
+++ src/main/java/com/webcheckers/model/Position.java
@@ -8,4 +8,29 @@ public class Position {
     // C# and properties are not a language defined existence.
     public int row;
     public int column;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + column;
+        result = prime * result + row;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Position other = (Position) obj;
+        if (column != other.column)
+            return false;
+        if (row != other.row)
+            return false;
+        return true;
+    }
 }
