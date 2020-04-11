84aa3c5b8ae322af1312d009c87a281138ce81a6
anb6536 <anb6536@rit.edu>
2020-04-11 12:30:02 -0400

    Completed testing BoardView class.

--- src/test/java/com/webcheckers/model/BoardViewTest.java
+++ src/test/java/com/webcheckers/model/BoardViewTest.java
@@ -18,10 +18,43 @@ public class BoardViewTest {
     void getSpaceTest(){
         int row = 0;
         int col = 1;
+
         Position position = Position.makePosition(row, col);
-        Space space = boardView.getSpace(position);
-        Assertions.assertEquals(space.getColor(), Space.Color.DARK);
-        Assertions.assertEquals(space.getPiece().getColor(), Piece.Color.WHITE );
-        Assertions.assertEquals(space.getPiece().getType(), Piece.Type.SINGLE );
+        Space space1 = boardView.getSpace(position);
+        Assertions.assertEquals(space1.getColor(), Space.Color.DARK);
+        Assertions.assertEquals(space1.getPiece().getColor(), Piece.Color.WHITE );
+        Assertions.assertEquals(space1.getPiece().getType(), Piece.Type.SINGLE );
+
+        Space space2 = boardView.getSpace(row, col);
+        Assertions.assertEquals(space2.getColor(), Space.Color.DARK);
+        Assertions.assertEquals(space2.getPiece().getColor(), Piece.Color.WHITE );
+        Assertions.assertEquals(space2.getPiece().getType(), Piece.Type.SINGLE );
+    }
+
+    @Test
+    void getRowTest(){
+        Assertions.assertNotNull(boardView.getRows());
+    }
+
+    @Test
+    void cloneTest(){
+        BoardView copyBoard = boardView.clone();
+        Assertions.assertEquals(boardView.getRows(), copyBoard.getRows());
+    }
+
+    @Test
+    void hasRedPieceTest(){
+        Assertions.assertTrue(boardView.hasRedPieces());
+
+        BoardView empty = Board.makeBoard().getBoardView();
+        Assertions.assertFalse(empty.hasRedPieces());
+    }
+
+    @Test
+    void hasWhitePieceTest(){
+        Assertions.assertTrue(boardView.hasWhitePieces());
+
+        BoardView empty = Board.makeBoard().getBoardView();
+        Assertions.assertFalse(empty.hasWhitePieces());
     }
 }
