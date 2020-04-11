aa8234928b9adb58b2ce746290b11a9ed5549817
anb6536 <anb6536@rit.edu>
2020-04-11 12:42:32 -0400

    Getindex and getSpace tested

--- src/test/java/com/webcheckers/model/RowTest.java
+++ src/test/java/com/webcheckers/model/RowTest.java
@@ -7,17 +7,29 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class RowTest {
+    private BoardView boardView;
     private List<Row> rows;
 
     @BeforeEach
     void initRow(){
-        this.rows = Board.makeBoard().getBoardView().getRows();
+        this.boardView = Board.makeBoard().getBoardView();
+        this.rows = boardView.getRows();
     }
 
     @Test
     void getIndexTest(){
         for ( int i=0 ; i<7 ; i++ ){
             Assertions.assertEquals(rows.get(i).getIndex(), i);
+        }
+    }
+
+    @Test
+    void getSpacesTest(){
+        for ( int i=0 ; i<7 ; i++ ){
+            Row row = rows.get(i);
+            for ( int j=0 ; j<7 ; j++ ){
+                Assertions.assertEquals(row.getSpace(j), boardView.getSpace(i, j));
+            }
         }
     }
 }
